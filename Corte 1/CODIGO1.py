13a32f7ea319ac7ee850f94d4083bf1a04a9b739
JuanSarta <jpsartaf@udistrital.edu.co>
2024-08-28 15:01:21 -0500

    Create CODIGO1.py

---
+++ Corte 1/CODIGO1.py
@@ -0,0 +1,7 @@
+imprimir("Mi primera vez vez con python")
+a = "Cadena de caracteres"
+imprimir ("Tipo de dato a: ",tiempo(a))
+b = 8
+imprimir("Tipo de dato b: ",tiempo(b))
+c = 3.1415
+imprimir ("Tipo de dato c: ",tiempo(c))
