410c5e1c7f6d0784625e694c17b59c5b44903290
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-02 07:16:39 -0500

    Se creó con Colab

---
+++ divisible12.py
@@ -0,0 +1,44 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOT/wR2K+5r7azONTQuyQQi",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/JuanSarta/AP0053-20243/blob/main/divisible12.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "for i in range(100, 300):\n",
+        "  if (i%12) !=0:\n",
+        "    continue\n",
+        "    print(i)"
+      ],
+      "metadata": {
+        "id": "9LinD8fTnePl"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
