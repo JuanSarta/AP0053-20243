101cf7b3fb8807a9cf5b3d2382c7519d55c1e34f
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:37:37 -0500

    number.py

--- Corte 1/Tuplas loops/number.py
+++ Corte 1/Tuplas loops/number.py
@@ -1,8 +1,8 @@
-import random
-from matplotlib import pyplot as plt
+import random  # Importa la librería 'random' para generar números aleatorios.
+from matplotlib import pyplot as plt  # Importa la función 'pyplot' de 'matplotlib' para crear gráficos.
 
 # Add your code below:
-numbers_a = range(1, 13)
-numbers_b = [random.randint(1, 1000) for i in range(12)]
-plt.plot(numbers_a, numbers_b)
-plt.show()
+numbers_a = range(1, 13)  # Crea una secuencia de números del 1 al 12.
+numbers_b = [random.randint(1, 1000) for i in range(12)]  # Genera una lista de 12 números aleatorios entre 1 y 1000.
+plt.plot(numbers_a, numbers_b)  # Crea un gráfico de líneas usando 'numbers_a' como eje x y 'numbers_b' como eje y.
+plt.show()  # Muestra el gráfico en pantalla.
