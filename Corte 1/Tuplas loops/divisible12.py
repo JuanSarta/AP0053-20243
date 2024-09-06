166b745c5cd5ab81913df3ed43d3edc3e27bf789
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:18:24 -0500

    divisible12.py

--- Corte 1/Tuplas loops/divisible12.py
+++ Corte 1/Tuplas loops/divisible12.py
@@ -1,4 +1,4 @@
-for i in range(100, 301):
-    if (i%12) != 0:
-        continue
-    print(i)
+for i in range(100, 301):  # Bucle 'for' que itera sobre los números del 100 al 300 (incluido 300)
+    if (i % 12) != 0:      # Comprueba si el número 'i' no es divisible por 12
+        continue           # Si el número no es divisible por 12, pasa a la siguiente iteración
+    print(i)               # Si el número es divisible por 12, lo imprime
