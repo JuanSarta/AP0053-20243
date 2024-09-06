08b6c67d372dbe94f677e01a3c6c1f50879f2700
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 16:24:56 -0500

    listsTuples.py

---
+++ Corte 1/dataStructures/listsTuples.py
@@ -0,0 +1,11 @@
+#Creación y manipulación básica de listas
+
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+print(my_lista)  # Imprime la lista completa
+print(type(my_lista))  # Imprime el tipo de dato (list)
+print(my_lista[2])  # Imprime el tercer elemento de la lista
+
+# Operaciones básicas sobre listas
+print("my_lista size: ", len(my_lista))  # Imprime la longitud de la lista
+print(my_lista[0:2])  # Imprime los primeros dos elementos (rango 0-2)
+print(my_lista[:2])  # Otra forma de imprimir los primeros dos elementos
