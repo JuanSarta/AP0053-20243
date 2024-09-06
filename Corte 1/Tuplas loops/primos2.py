325605949b7bd00768f5f97be399145d16851882
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:41:08 -0500

    primos2.py

--- Corte 1/Tuplas loops/primos2.py
+++ Corte 1/Tuplas loops/primos2.py
@@ -1,32 +1,34 @@
-a = 1
-value = input('Ingrese un valor')
-value = int(value)
+a = 1  
+value = input('Ingrese un valor')  # Solicita al usuario que ingrese un valor.
+value = int(value)  # Convierte el valor ingresado a un entero.
 
-while a == 1:
-    for i in range(1,value+1):
-        conta = 0
-        for n in range(1, i+1):
-            residue = i%n
-            if residue == 0:
-                conta = conta + 1
+while a == 1:  
+    for i in range(1, value + 1):  # Itera desde 1 hasta el valor ingresado por el usuario.
+        conta = 0 
+        for n in range(1, i + 1):  # Itera desde 1 hasta 'i' para encontrar los divisores de 'i'.
+            residue = i % n 
+            if residue == 0:  # Si el residuo es 0, significa que 'n' es divisor de 'i'.
+                conta = conta + 1  # Incrementa el contador de divisores.
             
-            # print("i = ", i)
-            # print("n = ", n)
-            # print("residue = ", residue)
-            # print("conta = ", conta)
-    if conta == 2:
-       print(f'{i} es un primo')
-       print("\n")
-    else:
-       print(f'{i} NOOO es un primo')
-       print("\n")
+            # Las siguientes líneas son comentarios que puedes descomentar para depuración:
+            # print("i = ", i) 
+            # print("n = ", n)  
+            # print("residue = ", residue)  # Imprime el residuo de 'i' % 'n'.
+            # print("conta = ", conta)  # Imprime el número de divisores actuales de 'i'.
 
-    print('Do you want to continue?. Press 1 to do that')
-    a = input()
-    a = int(a)
+    if conta == 2:  # Si 'i' tiene exactamente 2 divisores, es un número primo.
+        print(f'{i} es un primo')  # Imprime que 'i' es un número primo.
+        print("\n")  # Imprime un salto de línea para mayor claridad.
+    else:  # Si 'i' no tiene exactamente 2 divisores, no es primo.
+        print(f'{i} NOOO es un primo')  # Imprime que 'i' no es primo.
+        print("\n")  # Imprime un salto de línea.
 
-    if a != 1:
-        break
+    print('Do you want to continue?. Press 1 to do that')  # Pregunta si el usuario quiere continuar.
+    a = input()  
+    a = int(a)  
 
-    value = input('Ingrese un valor')
-    value = int(value)
+    if a != 1:  # Si el valor de 'a' no es 1, rompe el bucle y termina el programa.
+        break  # Sale del bucle 'while'.
+
+    value = input('Ingrese un valor')  # Si el usuario decide continuar, pide otro valor.
+    value = int(value)  # Convierte el nuevo valor ingresado a entero.
