548c0f540d4c803f704b869a9b7ed1ebe591f67b
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:30:13 -0500

    impares.py

--- Corte 1/Tuplas loops/impares.py
+++ Corte 1/Tuplas loops/impares.py
@@ -1,23 +1,26 @@
-# for i in range (1,21):
-#     residual = i%2
-#     if residual == 0:
-#         print(f'{i} is even')
+
+# for i in range(1, 21):  # Itera sobre los números del 1 al 20 (inclusive)
+#     residual = i % 2  # Calcula el residuo de la división de 'i' entre 2
+#     if residual == 0:  # Si el residuo es 0, el número es par
+#         print(f'{i} is even')  
 #     else:
-#         #print(f'{i} is odd')
-#         print(str(i) + ' is odd')
+#         #print(f'{i} is odd')  
+#         print(str(i) + ' is odd')  
 
-# for i in range (0,6):
-#     result = i**3
-#     print(result)
+# Segundo bloque comentado:
+# for i in range(0, 6):  # Itera sobre los números del 0 al 5
+#     result = i ** 3  # Eleva 'i' al cubo
+#     print(result)  
 
-times = input("Enter a number of times: ")
-times = float(times)
-times = int(times)
-print(type(times))
-print(times)
+# Tercer bloque:
+times = input("Enter a number of times: ")  # Solicita al usuario ingresar un número
+times = float(times)  # Convierte el valor ingresado en un número flotante
+times = int(times)  # Convierte el número flotante en un número entero
+print(type(times))  # Imprime el tipo de dato de 'times'
+print(times)  # Imprime el valor de 'times'
 
-if times == 0:
-    print("Don't do anything")
+if times == 0:  # Verifica si el valor de 'times' es 0
+    print("Don't do anything")  
 else:
-    for i in range(1,times+1):
-        print("i = ", i)
+    for i in range(1, times + 1):  # Si 'times' es mayor que 0, itera de 1 a 'times'
+        print("i = ", i)  # Imprime el valor actual de 'i' en cada iteración
