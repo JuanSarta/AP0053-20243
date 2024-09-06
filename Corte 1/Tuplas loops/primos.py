d8983780add79d5968c4cb61f7abec3bf5e9a480
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:38:20 -0500

    primos.py

--- Corte 1/Tuplas loops/primos.py
+++ Corte 1/Tuplas loops/primos.py
@@ -1,15 +1,15 @@
-import time
-inicio = time.time()
+import time  
+inicio = time.time()  # Guarda el tiempo de inicio antes de comenzar el cálculo.
 
-for i in range(0,31):
-    conta = 0
-    for n in range(1, i+1):
-        residue = i%n
-        if residue == 0:
-            conta = conta + 1
+for i in range(0, 31):  # Bucle que itera desde 0 hasta 30 (inclusive).
+    conta = 0  # Inicializa un contador para contar los divisores de 'i'.
+    for n in range(1, i+1):  # Bucle interno que va desde 1 hasta 'i', para verificar si 'n' es divisor de 'i'.
+        residue = i % n  # Calcula el residuo de la división de 'i' entre 'n'.
+        if residue == 0:  # Si el residuo es 0, 'n' es un divisor de 'i'.
+            conta = conta + 1  # Incrementa el contador de divisores.
               
-    if conta == 2:
-        print(f'{i} es un primo')
+    if conta == 2:  # Si 'i' tiene exactamente 2 divisores (solo divisible por 1 y por sí mismo), es un número primo.
+        print(f'{i} es un primo')  # Imprime que 'i' es un número primo.
         
-fin = time.time()
-print("t = ", (fin - inicio)*1000)
+fin = time.time()  # Guarda el tiempo al finalizar los cálculos.
+print("t = ", (fin - inicio) * 1000)  # Calcula y muestra el tiempo total de ejecución en milisegundos.
