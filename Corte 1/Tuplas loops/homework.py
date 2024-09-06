6b696559d73064cc55ad165c57dfa7622db8ae07
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:25:17 -0500

    homework.py

--- Corte 1/Tuplas loops/homework.py
+++ Corte 1/Tuplas loops/homework.py
@@ -1,19 +1,20 @@
-a = input("Enter a number: ")
-a = int(a)
-b = input("Enter b number: ")
-b = float(b)
-c = a + b
+a = input("Enter a number: ")  # Solicita al usuario un número y lo almacena como una cadena (string)
+a = int(a)  # Convierte la cadena ingresada en un número entero
+b = input("Enter b number: ")  # Solicita al usuario un segundo número y lo almacena como una cadena
+b = float(b)  # Convierte la cadena ingresada en un número de punto flotante (float)
 
-if a == b:
+c = a + b  
+
+if a == b: 
     print("equal")
 else:
-    print("Different")
+    print("Different") 
 
-print("Type of a is: ", type(a))
-print("Type of b is: ", type(b))
-print("c = ", c)
+print("Type of a is: ", type(a))  
+print("Type of b is: ", type(b))  
+print("c = ", c)  
 
-if type(a) == type(b):
-    print("a and b are of the same type")
+if type(a) == type(b):  # Comprueba si 'a' y 'b' son del mismo tipo de dato
+    print("a and b are of the same type")  
 else:
-    print("a and b are of different type")
+    print("a and b are of different type")  
