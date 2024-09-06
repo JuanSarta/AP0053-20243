ce8cb452a46b9702518775f2d230c6e01f37a19e
JuanSarta <jpsartaf@udistrital.edu.co>
2024-09-06 15:36:42 -0500

    loopFor.py

--- Corte 1/Tuplas loops/loopFor.py
+++ Corte 1/Tuplas loops/loopFor.py
@@ -1,10 +1,14 @@
-import time
+import time # Importamos la librería time, que nos permite manejar funciones relacionadas con el tiempo.
 
 cadena = 'Python'
 
-for letra in cadena:
-   if letra == 't':
-      continue
-   print(letra)
-   time.sleep(1)
+for letra in cadena: # Usamos un bucle for para recorrer cada letra de la variable 'cadena'.
+    # Verificamos si la letra actual es 't'.
+    if letra == 't':
+        # Si la letra es 't', usamos continue para saltar a la siguiente iteración sin ejecutar el código restante.
+        continue
+    # Si la letra no es 't', la imprimimos en la consola.
+    print(letra)
+    # Hacemos una pausa de 1 segundo antes de continuar con la siguiente letra.
+    time.sleep(1)
    
